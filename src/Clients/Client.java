18db39ee0e3938e2fadbcb58a1b3ab19d5fa76a1
Eliot Eloi <eliot.eloi22@gmail.com>
2025-10-21 20:08:05 -0300

    added Client

--- src/Clients/Client.java
+++ src/Clients/Client.java
@@ -5,6 +5,45 @@ public class Client {
     private int id;
     private String name;
     private String cellphone;
-//    private Categoria categoria;
+    private Category categoria;
+
+    public Client(int id, String name, String cellphone, Category categoria) {
+        this.id = id;
+        this.name = name;
+        this.cellphone = cellphone;
+        this.categoria = categoria;
+    }
+
+    public Category getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Category categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
