f95a72d0f153d9188b4ba8fd38335eabb9470891
Eliot Eloi <eliot.eloi22@gmail.com>
2025-10-22 18:10:50 -0300

    added toString

--- src/clients/CorporateClient.java
+++ src/clients/CorporateClient.java
@@ -1,9 +1,17 @@
 package clients;
 
 public class CorporateClient extends Client{
+
     private String cnpj;
 
     public CorporateClient(int id, String name, String cellphone, Category categoria, String cnpj) {
         super(id, name, cellphone, categoria);
+    }
+
+    @Override
+    public String toString() {
+        return "CorporateClient{" +
+                "cnpj='" + cnpj + '\'' +
+                '}';
     }
 }
