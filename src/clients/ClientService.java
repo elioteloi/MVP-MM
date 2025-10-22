853b6a2e1066288dc023ca0adffcdbefef2340f6
Eliot Eloi <eliot.eloi22@gmail.com>
2025-10-22 19:33:31 -0300

    addedfunction for  client to be added to hashmap and to retrieve it

--- src/clients/ClientService.java
+++ src/clients/ClientService.java
@@ -1,11 +1,25 @@
 package clients;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientService {
-    ArrayList<String> clients = new ArrayList<String>();
+    private final Map<Integer, Client> client;
 
-    public void cadstrarCliente(Client client) {
+    public ClientService() {
+        this.client = new HashMap<>();
+    }
 
+    public void addClient(Client client) {
+        if (this.client.containsKey(client.getId())) {
+            throw new RuntimeException("ID do cliente jah existe no cadastro");
+        }
+        this.client.put(client.getId(), client);
+    }
+
+
+    public Client getClient(int id) {
+        return this.client.get(id);
     }
 }
