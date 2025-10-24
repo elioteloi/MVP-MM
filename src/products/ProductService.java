a4614302b617a6825476c93c4085bf8395d90d86
Eliot Eloi <eliot.eloi22@gmail.com>
2025-10-23 21:32:18 -0300

    added edit product and list product function

--- src/products/ProductService.java
+++ src/products/ProductService.java
@@ -1,23 +1,48 @@
 package products;
+import clients.Client;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductService {
 
-    private List<Product> products = new ArrayList<>();
+    private final Map<Integer, Product> products;
+
+    public ProductService() {
+        this.products = new HashMap<>();
+    }
 
     public void addProduct(Product product) {
-        products.add(product);
+        if (this.products.containsKey(product.getId())) {
+            throw new RuntimeException("ID do cliente jah existe no cadastro");
+        }
+        this.products.put(product.getId(), product);
     }
 
-    public List<Product> getAllProducts() {
-        return products;
+    public Product getProduct(int id) {
+        return this.products.get(id);
     }
 
-    public Product getProductById(int id) {
-        return products.stream()
-                .filter(p -> p.getId() == id)
-                .findFirst()
-                .orElse(null);
+
+    public Product editProduct(int id, String name, String barCode, double price, double averageCost, int stock) {
+        this.products.get(id).setName(name);
+        this.products.get(id).setBarCode(barCode);
+        this.products.get(id).setPrice(price);
+        this.products.get(id).setAverageCost(averageCost);
+        this.products.get(id).setStock(stock);
+
+
+        return this.products.get(id);
     }
+
+    public void listProduct() {
+        for (Product product : products.values()) {
+            System.out.println(product);
+        }
+
+    }
+
+
 }
