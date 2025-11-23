d9c9113737f5a26ea6f955d142a963d85a97723b
Eliot Eloi <eliot.eloi22@gmail.com>
2025-11-22 23:29:55 -0300

    added functions to buttons of product view

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,6 +1,9 @@
 package view;
 
 
+import products.Product;
+import products.ProductService;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -15,15 +18,19 @@ public class MenuView extends JFrame {
 
         this.panel = new JPanel();
         this.panel.setLayout(new FlowLayout());
-        this.panel.setPreferredSize(new Dimension(500, 200));
+        this.panel.setPreferredSize(new Dimension(500, 800));
         add(this.panel);
 
-        createButton("Register", new ButtonRegisterHandler());
-
+        createButton("Add product", new ButtonRegisterProductHandler());
+        createButton("Get a product", new ButtonGetProductHandler());
+        createButton("Edit a product", new ButtonEditProductHandler());
+        createButton("Delete a product", new ButtonDeleteProductHandler());
+        createButton("Stock report", new ButtonStockReportHandler());
+        createButton("Sale report", new ButtonSaleReportHandler());
         createButton("Exit", new ButtonExitHandler());
 
         setSize(new Dimension(500, 300));
-        setPreferredSize(new Dimension(500, 200));
+        setPreferredSize(new Dimension(500, 800));
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -36,13 +43,52 @@ public class MenuView extends JFrame {
         this.panel.add(button);
     }
 
-    private static class ButtonRegisterHandler implements ActionListener {
+    private static class ButtonRegisterProductHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             new RegisterProductView();
 
         }
     }
+
+    private static class ButtonGetProductHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new GetProductView();
+
+        }
+    }
+    private static class ButtonEditProductHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new EditProductView();
+
+        }
+    }
+    private static class ButtonDeleteProductHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new DeleteProductView();
+
+        }
+    }
+
+    private static class ButtonStockReportHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new StockReport();
+
+        }
+    }
+
+    private static class ButtonSaleReportHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new SaleReport();
+
+        }
+    }
+
     private static class ButtonExitHandler implements ActionListener {
 
         @Override
